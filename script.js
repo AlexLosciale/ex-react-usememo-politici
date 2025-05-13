4ec5d0e6b90cc19430f0ca5d8bbb15b6763ffb33
AlexLosciale <losciale17@gmail.com>
2025-05-13 12:12:19 +0200

    add: react

---
+++ script.js
@@ -0,0 +1,30 @@
+const {usestate, useEffect} = React;
+
+function PoliticiansList() {
+    const [politicians, setPoliticians] = useState([]);
+  
+    useEffect(() => {
+      fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
+        .then((res) => res.json())
+        .then((data) => setPoliticians(data))
+        .catch((err) => console.error("Errore nel caricamento dei dati:", err));
+    }, []);
+
+    return (
+        <div>
+          <h1>Politici</h1>
+          <div className="politicians-container">
+            {politicians.map((p) => (
+              <div key={p.id} className="card">
+                <img src={p.image} alt={p.name} />
+                <h2>{p.name}</h2>
+                <p className="position">{p.position}</p>
+                <p>{p.biography}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+}
+
+ReactDOM.render(<PoliticiansList />, document.getElementById("root"));
