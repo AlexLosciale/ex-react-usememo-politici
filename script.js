4fdfffc7cc725aa8937425a364f866691a64f58a
AlexLosciale <losciale17@gmail.com>
2025-05-13 12:34:20 +0200

    add:mil1

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const {usestate, useEffect} = React;
-
 function PoliticiansList() {
-    const [politicians, setPoliticians] = useState([]);
-  
-    useEffect(() => {
-      fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
-        .then((res) => res.json())
-        .then((data) => setPoliticians(data))
-        .catch((err) => console.error("Errore nel caricamento dei dati:", err));
+    const [politicians, setPoliticians] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
+            .then((res) => res.json())
+            .then((data) => setPoliticians(data))
+            .catch((err) => console.error("Errore nel caricamento dei dati:", err));
     }, []);
 
     return (
         <div>
-          <h1>Politici</h1>
-          <div className="politicians-container">
-            {politicians.map((p) => (
-              <div key={p.id} className="card">
-                <img src={p.image} alt={p.name} />
-                <h2>{p.name}</h2>
-                <p className="position">{p.position}</p>
-                <p>{p.biography}</p>
-              </div>
-            ))}
-          </div>
+            <h1>Politici</h1>
+            <div className="politicians-container">
+                {politicians.map((p) => (
+                    <div key={p.id} className="card">
+                        <img src={p.image} alt={p.name} />
+                        <h2>{p.name}</h2>
+                        <p className="position">{p.position}</p>
+                        <p>{p.biography}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-      );
+    );
 }
 
 ReactDOM.render(<PoliticiansList />, document.getElementById("root"));
